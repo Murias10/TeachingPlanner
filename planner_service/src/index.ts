4f8748b5779fcb038a7520e2162551227e0f3b23
Murias10 <uo290009@uniovi.es>
2025-05-26 00:07:50 +0200

    Agregar soporte para MySQL y configurar rutas de usuario

--- planner_service/src/index.ts
+++ planner_service/src/index.ts
@@ -1,12 +1,38 @@
 import app from '@/app'
+import express from 'express'
 import { Request, Response } from 'express'
+import userRouter from '@/routes/user.routes'
+import { AppDataSource } from '@/config/data-source'
+
 
 const port = 3000
+
+app.use(express.json())
+
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (_req: Request, res: Response) => {
     res.send('Hello World!')
 })
 
+app.post('/api', (req: Request, res: Response) => {
+    const { name, email } = req.body
+    res.json({ name, email })
+}
+)
+
+app.use('/user', userRouter)
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+
+
+AppDataSource.initialize()
+    .then(() => {
+        console.log('📦 Data Source initialized');
+    })
+    .catch((err) => {
+        console.error('❌ Error during Data Source initialization:', err);
+    });

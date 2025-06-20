f9985fd23c1eb7bebd21c5b7db3ef5bb3944331b
Murias10 <uo290009@uniovi.es>
2025-06-20 15:20:13 +0200

    Hotfix 🛠️

--- planner_service/src/controllers/degree.controller.ts
+++ planner_service/src/controllers/degree.controller.ts
@@ -3,7 +3,7 @@ import { Request, Response } from 'express';
 import { AppDataSource } from '@/config/data-source';
 import { Degree } from '@/entities/degree.entity';
 
-export const getAllUsers = async (_req: Request, res: Response) => {
+export const getAllDegrees = async (_req: Request, res: Response) => {
     const users = await AppDataSource.getRepository(Degree).find();
     res.json(users);
 };

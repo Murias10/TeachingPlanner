9bf2636425400c95644cd68ada0973d4c7248736
Murias10 <uo290009@uniovi.es>
2025-09-29 23:43:49 +0200

    feat: renombrar bases de datos de 'planner_database' y 'management_database' a 'planner_db' y 'management_db'

--- management_database/schema.sql
+++ management_database/schema.sql
@@ -1,13 +1,13 @@
 -- Crear base de datos (opcional, si no la creaste)
-CREATE DATABASE IF NOT EXISTS management_database;
+CREATE DATABASE IF NOT EXISTS management_db;
 
 -- Crear usuario y darle acceso desde cualquier host (para contenedores)
 CREATE USER IF NOT EXISTS 'management_user'@'%' IDENTIFIED BY 'management_password';
 
 -- Dar permisos sobre la base
-GRANT ALL PRIVILEGES ON management_database.* TO 'management_user'@'%';
+GRANT ALL PRIVILEGES ON management_db.* TO 'management_user'@'%';
 
 -- Aplicar cambios
 FLUSH PRIVILEGES;
 
-USE management_database;
+USE management_db;
--- planner_database/schema.sql
+++ planner_database/schema.sql
@@ -1,16 +1,16 @@
 -- Crear base de datos (opcional, si no la creaste)
-CREATE DATABASE IF NOT EXISTS planner_database;
+CREATE DATABASE IF NOT EXISTS planner_db;
 
 -- Crear usuario y darle acceso desde cualquier host (para contenedores)
 CREATE USER IF NOT EXISTS 'planner_user'@'%' IDENTIFIED BY 'planner_password';
 
 -- Dar permisos sobre la base
-GRANT ALL PRIVILEGES ON planner_database.* TO 'planner_user'@'%';
+GRANT ALL PRIVILEGES ON planner_db.* TO 'planner_user'@'%';
 
 -- Aplicar cambios
 FLUSH PRIVILEGES;
 
-USE planner_database;
+USE planner_db;
 
 CREATE TABLE DEGREES (
     ID VARCHAR(36) NOT NULL,
